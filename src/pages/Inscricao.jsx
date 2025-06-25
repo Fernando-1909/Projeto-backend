5984de0a146bee3505713bed1d6697c7f0753ad8
Meni901 <meniramos3046@gmail.com>
2025-06-25 19:29:37 -0300

    Delete src directory

--- src/pages/Inscricao.jsx
+++
@@ -1,8 +0,0 @@
-export default function Inscricao() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Formulário de Inscrição</h1>
-      {/* Coloque aqui o formulário ou instruções para inscrição */}
-    </div>
-  );
-}
